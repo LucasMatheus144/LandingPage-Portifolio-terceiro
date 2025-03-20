36bb0f53b0f9ecedc13a686b0b8c904f517446b5
Lucas Matheus <lucasmatheussouza53@gmail.com>
2025-03-19 23:10:26 -0300

    Ajuste Brevo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,92 +1,101 @@
-    document.querySelectorAll('.navbar a').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault(); 
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); 
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
 
-            if (targetSection) {
-                slowScrollTo(targetSection.offsetTop, 700);
+        if (targetSection) {
+            slowScrollTo(targetSection.offsetTop, 700);
+        }
+    });
+});
+
+function slowScrollTo(targetPosition, duration) {
+    const startPosition = window.scrollY;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function easeInOutQuad(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("contactForm").addEventListener("submit", async function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+        const nome = formData.get("Nome");
+        const email = formData.get("Email");
+        const telefone = formData.get("Telefone");
+        const assunto = formData.get("Assunto");
+        const mensagem = formData.get("Message");
+
+        // Configurar payload do e-mail
+        const emailData = {
+            sender: { name: "Cliente Entrando em Contato pelo Site", email: "lucasmatheussouza53@gmail.com" },
+            to: [{ email: "lumeture4@gmail.com", name: "Dr.Glauco Marcelo Marques" }],
+            subject: assunto,
+            htmlContent: `
+                <h2>Informações preenchidas pelo cliente!</h2>
+                <p><strong>Nome:</strong> ${nome}</p>
+                <p><strong>Email:</strong> ${email}</p>
+                <p><strong>Telefone:</strong> ${telefone}</p>
+                <p><strong>Mensagem:</strong> ${mensagem}</p>
+            `,
+        };
+
+        try {
+            const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "api-key": "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", 
+                },
+                body: JSON.stringify(emailData),
+            });
+
+            if (response.ok) {
+                showAlert(); 
+                this.reset(); 
             }
-        });
+        } catch (error) {
+            console.error("Erro ao enviar e-mail:", error); 
+        }
     });
 
-    function slowScrollTo(targetPosition, duration) {
-        const startPosition = window.scrollY;
-        const distance = targetPosition - startPosition;
-        let startTime = null;
-
-        function animation(currentTime) {
-            if (startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime - startTime;
-            const run = easeInOutQuad(timeElapsed, startPosition, distance, duration);
-            window.scrollTo(0, run);
-
-            if (timeElapsed < duration) requestAnimationFrame(animation);
-        }
-
-        function easeInOutQuad(t, b, c, d) {
-            t /= d / 2;
-            if (t < 1) return (c / 2) * t * t + b;
-            t--;
-            return (-c / 2) * (t * (t - 2) - 1) + b;
-        }
-
-        requestAnimationFrame(animation);
-    }
-
-
-    document.addEventListener("DOMContentLoaded", function () {
-        const menuIcon = document.getElementById("menu-icon");
-        const navbar = document.querySelector(".navbar");
-
-        menuIcon.addEventListener("click", function () {
-            navbar.classList.toggle("active");
-        });
-    });
-
-
-    function setFormSubmitted() {
-        sessionStorage.setItem("formSubmitted", "true"); // Usa sessionStorage para manter o valor apenas até fechar o navegador
-    }
-    
-    function checkFormSubmission() {
-        console.log("Verificando envio do formulário...");
-    
-        if (sessionStorage.getItem("formSubmitted") === "true") {
-            console.log("Exibindo pop-up de sucesso");
-            showAlert();
-            sessionStorage.removeItem("formSubmitted"); // Remove o status após exibir o pop-up
-        }
-    }
-    
     function showAlert() {
         var alerta = document.getElementById('Alerta');
         if (alerta) {
-            alerta.style.display = 'flex'; // Exibe o alerta
-            alerta.classList.add('active'); // Adiciona classe de exibição
-    
-            // Fecha automaticamente após 8 segundos
+            alerta.style.display = 'flex'; 
+            alerta.classList.add('active'); 
+
             setTimeout(hideAlert, 8000);
         }
     }
-    
+
     function hideAlert() {
         var alerta = document.getElementById('Alerta');
         if (alerta) {
-            alerta.classList.remove('active'); // Inicia a animação de fechamento
+            alerta.classList.remove('active');
             setTimeout(() => {
                 alerta.style.display = 'none';
-            }, 500); // Aguarda a conclusão da animação antes de ocultar completamente
+            }, 500); 
         }
     }
-    
-    // Fecha o alerta manualmente ao clicar no botão de fechar
-    document.addEventListener('DOMContentLoaded', () => {
-        document.querySelectorAll('.custom-alerta .close').forEach(icon => {
-            icon.addEventListener('click', hideAlert);
-        });
-    
-        checkFormSubmission(); // Verifica se o formulário foi enviado ao carregar a página
-    });
-    
+});
