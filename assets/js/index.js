71a3a7117023b9c178641f11d851029f8fccecd7
Lucas Marques <lucasmatheussouza53@gmail.com>
2025-03-28 10:23:22 -0300

    Ajuste txt typing

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Configurar payload do e-mail
         const emailData = {
             sender: { name: "Cliente Entrando em Contato pelo Site", email: "lucasmatheussouza53@gmail.com" },
-            to: [{ email: "lumeture4@gmail.com", name: "Dr.Glauco Marcelo Marques" }],
+            to: [{ email: "glaucomarcelo@aconsel.net", name: "Dr.Glauco Marcelo Marques" }],
             subject: assunto,
             htmlContent: `
                 <h2>Informações preenchidas pelo cliente!</h2>
@@ -99,3 +99,85 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navbar = document.querySelector(".navbar");
+
+    menuIcon.addEventListener("click", function (event) {
+        navbar.classList.toggle("active");
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!navbar.contains(event.target) && !menuIcon.contains(event.target)) {
+            navbar.classList.remove("active");
+        }
+    });
+});
+
+function hideAlert() {
+    var alerta = document.getElementById('Alerta');
+    if (alerta) {
+        alerta.classList.remove('active'); 
+        setTimeout(() => {
+            alerta.style.display = 'none';
+        }, 500);
+    }
+}
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #8D6E63}";
+        document.body.appendChild(css);
+    };
